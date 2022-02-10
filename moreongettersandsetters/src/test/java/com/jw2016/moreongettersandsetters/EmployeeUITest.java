127065c4aee10619a8f2bad548d3a80083bd234f
hoiwanjohnlouis <hoiwanjohnlouis@gmail.com>
2022-02-10 00:54:05 -0500

    20220210-1 1. added junit-4.13, added log4j-1.2.17, add test case infrastructure

---
+++ moreongettersandsetters/src/test/java/com/jw2016/moreongettersandsetters/EmployeeUITest.java
@@ -0,0 +1,17 @@
+package com.jw2016.moreongettersandsetters;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class EmployeeUITest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
